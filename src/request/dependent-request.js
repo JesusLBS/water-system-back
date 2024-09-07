e5682b11b6c2280d70574c70d36b9c23105c2f2a
Jesus Chicho Hernández <chichohdzjesus@gmail.com>
2024-09-06 19:23:43 -0600

    feat: :sparkles: Request.
    Parameter request validations.

---
+++ src/request/dependent-request.js
@@ -0,0 +1,17 @@
+const { check } = require('express-validator');
+const ValidateHelper = require('../helpers/validation/ validateHelper');
+const errorValueParameter = 'The parameter is required';
+const valid = new ValidateHelper();
+
+exports.DependentRequest = [
+  check('socioUid', errorValueParameter)
+    .exists()
+    .custom((value) => valid.isValid(value)),
+  check('dependents', errorValueParameter)
+    .exists()
+    .custom((value) => valid.isValid(value)),
+];
+
+exports.DependentValidation = (req, res, next) => {
+  valid.validationResult(req, res, next);
+};
